6df14c5df2a09f31283a77a6d2d8c49a3c8508d2
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-06-30 13:18:41 +0300

    выполнили задачу 36

---
+++ задача 36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] getArray(){
+    int[] array = new int[4];
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next(1, 101);
+    }
+    System.Console.WriteLine("[" + String.Join(", ", array) + "]");
+    return array;
+}
+
+int getSum(int[] array) {
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++) {
+        if (i % 2 > 0) {
+            sum = i + sum;
+        }
+    } 
+    return sum; 
+}
+
+void main() {
+    int[] Array = getArray();
+    int Sum = getSum(Array);
+    System.Console.WriteLine(Sum);   
+}
+
+main();
+
